52d2721453f7df79740dbe868086b3a1f9f1ac55
Denzel Banegas <denzelbanegas@gmail.com>
2021-01-26 22:27:45 +0000

    Version 1.0

--- SeachApp/ContentView.swift
+++ SeachApp/ContentView.swift
@@ -9,72 +9,36 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    @State var filteredItems = apps
+    
     var body: some View {
-        List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+
+        CustomNavigationView(view: AnyView(Home(filteredItems: $filteredItems)), placeHolder: "Apps", largeTitle: true, title: "Search",
+                             
+            onSearch: { (txt) in
+
+            // filterting Data...
+            if txt != ""{
+                self.filteredItems = apps.filter{$0.name.lowercased().contains(txt.lowercased())}
             }
-            .onDelete(perform: deleteItems)
-        }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
-
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
+            else{
+                self.filteredItems = apps
             }
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+            
+        }, onCancel: {
+            // Do Your Own Code When Search And Canceled....
+            self.filteredItems = apps
+            
+        })
+        .ignoresSafeArea()
     }
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
+
+// Custom Navigation View.....
+
---
+++ SeachApp/NavigationSearchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSearchBarApp.swift
+//  SeachApp
+//
+//  Created by denzel banegas on 26/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSearchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
